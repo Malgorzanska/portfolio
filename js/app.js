a1c3a01290ec8435161303dd5b65f46f1b113460
Małgorzata Dobrzańska <malgorzata.dobrzanska1@gmail.com>
2019-02-07 15:40:03 +0100

    js header

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@
 $(document).ready(function () {
 
     //pre-loader
+    const $containerPreloader = $(".container-preloader");
 
     $(window).on('load',(function () {
         setTimeout(
             function(){
-            $(".container-preloader").fadeOut();
+                $containerPreloader.fadeOut();
         }, 3000);
+    }));
+
+    //hamburger menu
+    const $hamburgerWrapper = $(".hamburger-wrapper");
+    const $navigation = $(".nav-list");
+
+    $hamburgerWrapper.on("click", (function () {
+        $hamburgerWrapper.toggleClass("active");
+        $navigation.toggleClass("show-menu");
     }));
 
 });
